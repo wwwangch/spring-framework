9de27e424881b25872dae9940cbf29d977831a06
wangchenhui <874481480wch>
2022-07-29 17:11:15 +0800

    5.3.x build test

---
+++ spring-wch-test/src/main/java/com/wch/study/spring/service/AService.java
@@ -0,0 +1,16 @@
+package com.wch.study.spring.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author wch
+ * @Version 1.0
+ * @Date 2022/7/29 17:03
+ */
+@Service
+public class AService {
+	@Override
+	public String toString() {
+		return "AService";
+	}
+}
